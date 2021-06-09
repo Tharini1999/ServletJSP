17b65272028b2be35a746a940a5faf0d277c0c1d
Bhavatarini <Bhavatarini@LAPTOP-HCIEIDR7>
2021-06-09 12:52:26 +0530

    First Commit
    Uploading All files of DemoApp

---
+++ ServletJSP/DemoApp/src/main/java/com/tharini/AddServlet.java
@@ -0,0 +1,24 @@
+package com.tharini;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet{
+	
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int result = i+j;
+		
+		PrintWriter out = res.getWriter();
+		out.println("The Result is : "+result);
+		
+	}
+}
+
