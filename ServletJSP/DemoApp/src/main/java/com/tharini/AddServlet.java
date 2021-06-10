c7b1d61327e73c3c2c4d15a15d6c1d8e8713a366
Bhavatarini <Bhavatarini@LAPTOP-HCIEIDR7>
2021-06-10 12:55:31 +0530

    Commit 3
    Session and Cookies

--- ServletJSP/DemoApp/src/main/java/com/tharini/AddServlet.java
+++ ServletJSP/DemoApp/src/main/java/com/tharini/AddServlet.java
@@ -5,9 +5,11 @@ import java.io.PrintWriter;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet{
 	
@@ -16,10 +18,27 @@ public class AddServlet extends HttpServlet{
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
-		int result = i+j;
-		req.setAttribute("result",result);
-		RequestDispatcher rq = req.getRequestDispatcher("sq");
-		rq.forward(req, res);
+		int k = i+j;
+		
+		
+		/*Cookies*/
+		Cookie cookie = new Cookie("k",k+"");
+		res.addCookie(cookie);
+		res.sendRedirect("sq");
+		
+		/*SESSION*/
+		//HttpSession session = req.getSession();
+		//session.setAttribute("k",k);
+		//res.sendRedirect("sq");
+		
+		/*URL REWRITING*/
+		//res.sendRedirect("sq?k="+k); //url rewriting
+		
+		
+		/*REQUEST DISPATCHER*/
+		//req.setAttribute("result",result);
+		//RequestDispatcher rq = req.getRequestDispatcher("sq");
+		//rq.forward(req, res);
 		
 	}
 }
--- ServletJSP/DemoApp/src/main/java/com/tharini/sqServlet.java
+++ ServletJSP/DemoApp/src/main/java/com/tharini/sqServlet.java
@@ -3,15 +3,34 @@ package com.tharini;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class sqServlet extends HttpServlet{
 
 	public void service (HttpServletRequest req, HttpServletResponse res) throws IOException
 	{
-		int k = (int)req.getAttribute("result");
+		
+		
+		int k =0;
+		/*SESSION*/
+		//HttpSession session = req.getSession();
+		//k=(int)session.getAttribute("k");
+		
+		
+		Cookie[] cookie = req.getCookies();
+		
+		for(Cookie c : cookie)
+		{
+			if(c.getName().equals("k")==true)
+			{
+				k=Integer.parseInt(c.getValue());
+			}
+		}
+		
 		k=k*k;
 		PrintWriter out = res.getWriter();
 		out.println("The square of sum = "+k);
